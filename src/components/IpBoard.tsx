add64cebe74107334da11e303b5fe78cb8156f80
belunatic <abelluvyou@yahoo.com>
2025-11-19 21:13:47 -0800

    feat: created a board to hold all the components

---
+++ src/components/IpBoard.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import FormInput from "./FormInput";
+import IpData from "./IpData";
+import IpMap from "./IpMap";
+
+export default function IpBoard() {
+	const [options, setOptions] = useState<string>("");
+	const [ipData, setIpData] = useState(null);
+
+	return (
+		<>
+			<header className="py-8 overflow-visible h-[400px] md:h-[300px] overflow-visible relative z-9999 bg-[url(./assets/images/pattern-bg-mobile.png)] md:bg-[url(./assets/images/pattern-bg-desktop.png)] bg-cover bg-center">
+				<div className="flex flex-col gap-y-8 text-center max-w-[85rem] justify-center mx-auto p-8">
+					<h1 className="text-2xl md:text-3xl text-white font-bold">
+						IP Address Tracker
+					</h1>
+					<FormInput setOptions={setOptions} />
+					{/* <IpData /> */}
+				</div>
+			</header>
+
+			{/* <div id="map" className="h-full flex-grow"></div> */}
+			<IpMap />
+		</>
+	);
+}
